9cde86082a3e38e3ecbebbdc7bc079d57bf48ad9
ridho-dev <ridhopardede.dev@gmail.com>
2022-07-28 09:46:21 +0700

    Add: Detail data and back button

--- app/src/main/java/com/dededev/moviehero/detail/DetailActivity.kt
+++ app/src/main/java/com/dededev/moviehero/detail/DetailActivity.kt
@@ -2,12 +2,37 @@ package com.dededev.moviehero.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.dededev.moviehero.R
+import com.dededev.moviehero.core.domain.model.Movie
+import com.dededev.moviehero.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.detailToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val movie = intent.getParcelableExtra<Movie>(EXTRA_DATA)
+        showSelectedMovie(movie)
+    }
+
+    private fun showSelectedMovie(movie: Movie?) {
+        movie?.let {
+            binding.tvDetailTitle.text = movie.title
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
