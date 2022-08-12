75ac79322d1556fa2f150c2baf6ae2c7fb320e1f
ridho-dev <ridhopardede.dev@gmail.com>
2022-08-12 11:35:11 +0700

    Add: Obfuscation

--- core/src/main/java/com/dededev/core/data/source/local/room/MovieDatabase.kt
+++ core/src/main/java/com/dededev/core/data/source/local/room/MovieDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.dededev.core.data.source.local.entity.MovieEntity
 import com.dededev.core.data.source.local.entity.SearchedMovieEntity
 
-@Database(entities = [MovieEntity::class, SearchedMovieEntity::class], version = 1)
+@Database(entities = [MovieEntity::class, SearchedMovieEntity::class], version = 1, exportSchema = false)
 abstract class MovieDatabase: RoomDatabase() {
     abstract fun movieDao(): MovieDao
 
